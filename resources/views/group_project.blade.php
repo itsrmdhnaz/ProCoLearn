9ad1b140865c7f587bac27838b49801eafc9d93f
Muhammad Dhafa Ramadhani <dhafaramadhani2004@gmail.com>
2024-12-16 20:32:50 +0700

    responsive group project

--- resources/views/group_project.blade.php
+++ resources/views/group_project.blade.php
@@ -14,7 +14,7 @@
     <!-- Main Container -->
     <div class="h-screen max-w-screen-2xl mx-auto overflow-hidden bg-lightDark flex flex-col">
         <!-- Header Section -->
-        <div class="flex w-full gap-3 flex-row my-4 px-4">
+        <div class="flex w-full gap-3 flex-row my-4 sm:hidden px-4">
             {{-- logo in mobile --}}
             <img src="{{ asset('assets/images/logo-mobile.svg') }}" alt="logo" class="sm:hidden w-24">
             {{-- search --}}
@@ -33,7 +33,7 @@
         </div>
 
         <!-- Filter Button -->
-        <div class="px-4 py-3 flex justify-end">
+        <div class="px-4 py-3 flex justify-end sm:hidden">
             <button
                 class="bg-orangeCustom text-white font-semibold px-4 py-1 rounded-lg flex items-center space-x-2 drop-shadow-xl">
                 <span>filter</span>
@@ -41,90 +41,117 @@
             </button>
         </div>
 
-        <!-- Scrollable Content Area -->
-        <div class="flex-1 overflow-y-auto scrolled-text px-4">
-            <div class="space-y-4">
-                <div class="h-4"></div>
-                <!-- Card 1 -->
-                <div class="bg-dark rounded-3xl p-2">
-                    <div class="bg-lightDark rounded-t-3xl h-36 mb-2">
-                        <span
-                            class="inline-block m-4 bg-orangeCustom bg-opacity-70 drop-shadow-lg text-white px-3 py-1 rounded-full text-sm mb-4">
-                            not-started
-                        </span>
-                    </div>
-                    <div class="space-y-2 mx-3">
-                        <div class="flex justify-between">
-                            <h3 class="text-white text-xl font-semibold">To Do APP</h3>
-                            <span class="text-green-400">Needs 3</span>
+
+        <div class="flex-1 overflow-y-auto scrolled-text sm:overflow-hidden sm:flex sm:h-screen">
+            <x-side-bar-project></x-side-bar-project>
+            <!-- Scrollable Content Area -->
+            <div class="sm:flex-1 scrolled-text px-4 overflow-y-auto scrolled-text sm:overflow-hidden">
+                <div class="hidden sm:flex mt-4 gap-2 items-center">
+                    {{-- search --}}
+                    <div class="flex flex-1 justify-end" id="searchContainer">
+                        <div class="flex gap-2 items-center transition-all duration-100 border-0 ease-in-out rounded-none py-0 px-0 sm:rounded-full sm:border-white sm:border-2 sm:py-2 sm:px-4 sm:gap-2 sm:flex-1"
+                            id="searchWrapper">
+                            <img src="{{ asset('assets/images/search-mini.svg') }}" alt="logo"
+                                class="w-7 cursor-pointer sm:cursor-default" id="searchIcon">
+                            <input type="text" placeholder="Search Your Study Group Here"
+                                class="hidden bg-transparent w-0 sm:block sm:w-full placeholder-[#D9D9D9] border-0 p-0 focus:outline-none focus:ring-0 text-white caret-white transition-all duration-300"
+                                id="searchInput">
                         </div>
-                        <p class="text-[#D9D9D9] w-1/2 overflow-hidden text-ellipsis whitespace-nowrap">Flutter,
-                            Laravel, UI</p>
-                        <div class="flex justify-between items-center h-8"> <!-- Tambahkan h-8 di sini -->
-                            <div class="flex items-center gap-2 h-full"> <!-- Tambahkan h-full di sini -->
-                                <div class="relative w-8 h-6"> <!-- Tambahkan h-6 di sini -->
-                                    <!-- Background F -->
-                                    <div class="absolute left-2 w-6 h-6 bg-orange-400 rounded-full"></div>
-                                    <!-- Main F -->
-                                    <div
-                                        class="absolute left-0 w-6 h-6 bg-yellow-500 rounded-full flex items-center justify-center">
-                                        <span class="text-white text-sm">F</span>
+                    </div>
+                    {{-- filter --}}
+                    <i
+                        class="ti ti-filter-search font-normal text-3xl text-center text-white h-auto w-10 justify-center bg-dark hidden sm:flex items-center rounded-lg flex-shrink-0"></i>
+                </div>
+                <p class="text-white text-2xl font-semibold mt-4 ml-4 hidden sm:block">New Project Groups Released</p>
+                {{-- nearby project --}}
+                <div class="space-y-4 sm:space-x-4 sm:flex sm:overflow-x-auto scrolled-text">
+                    <div class="h-4 sm:h-0"></div>
+                    @for ($i = 0; $i < 5; $i++)
+                        <div class="bg-dark rounded-3xl p-2 max-w-sm min-w-80 mx-auto">
+                            <div class="bg-lightDark rounded-t-3xl h-36 mb-2">
+                                <span
+                                    class="inline-block m-4 bg-orangeCustom bg-opacity-70 drop-shadow-lg text-white px-3 py-1 rounded-full text-sm mb-4">
+                                    not-started
+                                </span>
+                            </div>
+                            <div class="space-y-2 mx-3">
+                                <div class="flex justify-between">
+                                    <h3 class="text-white text-xl font-semibold">To Do APP</h3>
+                                    <span class="text-green-400">Needs 3</span>
+                                </div>
+                                <p class="text-[#D9D9D9] w-1/2 overflow-hidden text-ellipsis whitespace-nowrap">Flutter,
+                                    Laravel, UI</p>
+                                <div class="flex justify-between items-center h-8"> <!-- Tambahkan h-8 di sini -->
+                                    <div class="flex items-center gap-2 h-full"> <!-- Tambahkan h-full di sini -->
+                                        <div class="relative w-8 h-6"> <!-- Tambahkan h-6 di sini -->
+                                            <!-- Background F -->
+                                            <div class="absolute left-2 w-6 h-6 bg-orange-400 rounded-full"></div>
+                                            <!-- Main F -->
+                                            <div
+                                                class="absolute left-0 w-6 h-6 bg-yellow-500 rounded-full flex items-center justify-center">
+                                                <span class="text-white text-sm">F</span>
+                                            </div>
+                                        </div>
+                                        <span class="text-[#D9D9D9] text-sm">2 more</span>
+                                    </div>
+                                    <div class="flex items-center space-x-3">
+                                        <!-- Bookmark Icon -->
+                                        <i class="ti ti-bookmark text-orangeCustom text-2xl"></i>
+                                        <i class="ti ti-rosette-discount-check-off text-orangeCustom text-2xl"></i>
                                     </div>
                                 </div>
-                                <span class="text-[#D9D9D9] text-sm">2 more</span>
-                            </div>
-                            <div class="flex items-center space-x-3">
-                                <!-- Bookmark Icon -->
-                                <i class="ti ti-bookmark text-orangeCustom text-2xl"></i>
-                                <i class="ti ti-rosette-discount-check-off text-orangeCustom text-2xl"></i>
                             </div>
                         </div>
-                    </div>
+                    @endfor
+                    <div class="h-16"></div>
                 </div>
-
-                <!-- Card 2 -->
-                <div class="bg-dark rounded-3xl p-2">
-                    <div class="bg-lightDark rounded-t-3xl h-36 mb-2">
-                        <span
-                            class="inline-block m-4 bg-orangeCustom bg-opacity-70 drop-shadow-lg text-white px-3 py-1 rounded-full text-sm mb-4">
-                            not-started
-                        </span>
-                    </div>
-                    <div class="space-y-2 mx-3">
-                        <div class="flex justify-between">
-                            <h3 class="text-white text-xl font-semibold">To Do APP</h3>
-                            <span class="text-green-400">Needs 3</span>
-                        </div>
-                        <p class="text-[#D9D9D9] w-1/2 overflow-hidden text-ellipsis whitespace-nowrap">Flutter,
-                            Laravel, UI</p>
-                        <div class="flex justify-between items-center h-8"> <!-- Tambahkan h-8 di sini -->
-                            <div class="flex items-center gap-2 h-full"> <!-- Tambahkan h-full di sini -->
-                                <div class="relative w-8 h-6"> <!-- Tambahkan h-6 di sini -->
-                                    <!-- Background F -->
-                                    <div class="absolute left-2 w-6 h-6 bg-orange-400 rounded-full"></div>
-                                    <!-- Main F -->
-                                    <div
-                                        class="absolute left-0 w-6 h-6 bg-yellow-500 rounded-full flex items-center justify-center">
-                                        <span class="text-white text-sm">F</span>
+                <p class="text-white text-2xl font-semibold mt-4 ml-4 hidden sm:block">Top Project Groups</p>
+                <div class="space-y-4 space-x-4 hidden sm:flex overflow-x-auto scrolled-text">
+                    <div class="h-4 sm:h-0"></div>
+                    @for ($i = 0; $i < 5; $i++)
+                        <div class="bg-dark rounded-3xl p-2 max-w-sm min-w-80 mx-auto">
+                            <div class="bg-lightDark rounded-t-3xl h-36 mb-2">
+                                <span
+                                    class="inline-block m-4 bg-orangeCustom bg-opacity-70 drop-shadow-lg text-white px-3 py-1 rounded-full text-sm mb-4">
+                                    not-started
+                                </span>
+                            </div>
+                            <div class="space-y-2 mx-3">
+                                <div class="flex justify-between">
+                                    <h3 class="text-white text-xl font-semibold">To Do APP</h3>
+                                    <span class="text-green-400">Needs 3</span>
+                                </div>
+                                <p class="text-[#D9D9D9] w-1/2 overflow-hidden text-ellipsis whitespace-nowrap">Flutter,
+                                    Laravel, UI</p>
+                                <div class="flex justify-between items-center h-8"> <!-- Tambahkan h-8 di sini -->
+                                    <div class="flex items-center gap-2 h-full"> <!-- Tambahkan h-full di sini -->
+                                        <div class="relative w-8 h-6"> <!-- Tambahkan h-6 di sini -->
+                                            <!-- Background F -->
+                                            <div class="absolute left-2 w-6 h-6 bg-orange-400 rounded-full"></div>
+                                            <!-- Main F -->
+                                            <div
+                                                class="absolute left-0 w-6 h-6 bg-yellow-500 rounded-full flex items-center justify-center">
+                                                <span class="text-white text-sm">F</span>
+                                            </div>
+                                        </div>
+                                        <span class="text-[#D9D9D9] text-sm">2 more</span>
+                                    </div>
+                                    <div class="flex items-center space-x-3">
+                                        <!-- Bookmark Icon -->
+                                        <i class="ti ti-bookmark text-orangeCustom text-2xl"></i>
+                                        <i class="ti ti-rosette-discount-check-off text-orangeCustom text-2xl"></i>
                                     </div>
                                 </div>
-                                <span class="text-[#D9D9D9] text-sm">2 more</span>
-                            </div>
-                            <div class="flex items-center space-x-3">
-                                <!-- Bookmark Icon -->
-                                <i class="ti ti-bookmark text-orangeCustom text-2xl"></i>
-                                <i class="ti ti-rosette-discount-check-filled text-orangeCustom text-2xl"></i>
                             </div>
                         </div>
-                    </div>
+                    @endfor
                 </div>
-
-                <div class="h-16"></div>
             </div>
         </div>
 
+
         <!-- Bottom Navigation -->
-        <div class="bg-dark p-4 flex justify-around items-end">
+        <div class="bg-dark p-4 flex justify-around items-end sm:hidden">
             <button>
                 <i class="ti ti-world-code text-3xl text-lightDark2"></i>
             </button>
